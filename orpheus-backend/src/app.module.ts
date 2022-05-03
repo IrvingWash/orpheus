3c63ed3d0cbb54be76c41c213bad34a4ca7ad436
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2022-05-03 21:05:06 +0300

    add static serving

--- orpheus-backend/src/app.module.ts
+++ orpheus-backend/src/app.module.ts
@@ -1,5 +1,7 @@
+import * as path from 'path';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { FileModule } from './file/file.module';
 import { TrackModule } from './track/track.module';
@@ -9,6 +11,9 @@ import { TrackModule } from './track/track.module';
 		MongooseModule.forRoot('mongodb://127.0.0.1:27017/orpheus'),
 		TrackModule,
 		FileModule,
+		ServeStaticModule.forRoot({
+			rootPath: path.resolve(__dirname, 'static'),
+		}),
 	],
 })
 export class AppModule {}
