aaf2ec24deabbe554c0283f5490cf93d055c9df7
Armen Gulakhszyan <agulakhszyan@tradingview.com>
2022-05-03 21:55:41 +0300

    configure frontend project

---
+++ orpheus-frontend/.eslintrc.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir : __dirname, 
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+	],
+	root: true,
+	env: {
+		node: true,
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	rules: {
+		'eol-last': ['error'],
+		'object-curly-spacing': ['error', 'always'],
+		'comma-dangle': ['error', 'always-multiline'],
+		'@typescript-eslint/explicit-function-return-type': ['error'],
+		'@typescript-eslint/explicit-member-accessibility': ['error'],
+		'semi': ['error', 'always'],
+	},
+};
